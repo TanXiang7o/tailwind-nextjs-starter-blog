68249866bd0a93d77750f1e0b6fa0e2e3991626f
tanxiang <1092145181@qq.com>
2024-08-19 22:30:16 +0800

    test umami

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app cloud.umami.is us.umami.is;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *;
-  connect-src *;
+  connect-src * cloud.umami.is us.umami.is;
   font-src 'self';
   frame-src giscus.app
 `
